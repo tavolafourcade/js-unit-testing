75ad704fc2ddc621e9b62265a015d5b745655339
tavolafourcade <tavolafourcade@gmail.com>
2022-08-01 22:18:20 -0500

    [ ADD ] Improving the test with the AAA pattern (Arrange, Act, Assert)

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,6 +3,11 @@ import { add } from './math'
 // First argument: Define what the test will test in a short sentence.
 // Second argument: Define the test function (the actual test). We will write the actual testing code here.
 it('should summarize all number values in an array', ()=> {
-  const result = add([1,2,3,4,5])
-  expect(result).toBe(15)
+  // Arrange
+  const numbers = [1,2,3,4,5]
+  // Act
+  const result = add(numbers)
+  // Assert
+  const expectedResult = numbers.reduce((prevValue, curValue)=> prevValue + curValue, 0)
+  expect(result).toBe(expectedResult)
 })
