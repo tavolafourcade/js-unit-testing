fff6da2f4853cec3f1bb71dff7371a88ace6d456
tavolafourcade <tavolafourcade@gmail.com>
2022-08-03 21:42:19 -0500

    [ ADD ] Multiple expectations

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -4,11 +4,23 @@ import { transformToNumber } from './numbers'
 it('should transform a string number to a number of type number', () => {
   const value = '1'
   const result = transformToNumber(value)
+  expect(result).toBe(+value)
+})
+
+it('should transform a string number to a number of type number', () => {
+  const value = '1'
+  const result = transformToNumber(value)
   expect(result).toBeTypeOf('number')
 })
 
 it('should yield NaN for non-transformable values', () => {
   const value = 'hola'
+  const value2 = {}
+
   const result = transformToNumber(value)
+  const result2 = transformToNumber(value2)
+
+  // Multiple expectations does make sense here
   expect(result).toBeNaN()
+  expect(result2).toBeNaN()
 })
