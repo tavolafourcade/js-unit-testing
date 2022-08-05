ebaf2a183131a1654eb80354dfb95f685a524946
tavolafourcade <tavolafourcade@gmail.com>
2022-08-05 18:59:35 -0500

    [ ADD ] Refactor formSubmitHandler

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,3 +1,4 @@
+import { cleanNumbers } from './util/numbers.js'
 export function add(numbers) {
   let sum = 0;
 
@@ -5,4 +6,16 @@ export function add(numbers) {
     sum += +number; // + force to convert string to number
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  let result = ''
+  try {
+    const numbers = cleanNumbers(numberValues);
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+
+  return result
 }
---
+++ basic-testing-frontend/src/util/output.js
@@ -0,0 +1,18 @@
+
+export function generateResultText(calculationResult) {
+  let resultText = '';
+
+  if (result === 'invalid') {
+    resultText = 'Invalid input. You must enter valid numbers.';
+  } else if (calculationResult !== 'no-calc') {
+    resultText = 'Result: ' + calculationResult;
+  }
+
+  return resultText
+}
+
+export function outputResult(resultText){
+const output = document.getElementById('result');
+
+  output.textContent = resultText;
+}
