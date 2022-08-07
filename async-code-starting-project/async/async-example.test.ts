f6b4936903e505fbca7a053fb1907dd4fe490fa7
tavolafourcade <tavolafourcade@gmail.com>
2022-08-07 16:44:36 -0500

    [ ADD ] Testing Asynchronous Code with Promises & Async/await

--- async-code-starting-project/async/async-example.test.ts
+++ async-code-starting-project/async/async-example.test.ts
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest'
-import { generateToken } from './async-example'
+import { generateToken, generateTokenPromise } from './async-example'
 
-it('should generate a token value ', (done)=> {
+it('should generate a token value', (done)=> {
   const testUserEmail = 'test@test.com'
 
   generateToken(testUserEmail, (err, token) => {
@@ -13,4 +13,22 @@ it('should generate a token value ', (done)=> {
       done(error)
     }
   })
-  })
+})
+
+// First way for testing promises
+it('should generate a token value', () => {
+  const testUserEmail = 'test@test.com'
+
+  const token = generateTokenPromise(testUserEmail)
+
+  expect(token).resolves.toBeDefined()
+})
+
+// Second way for testing promises
+it('should generate a token value', async () => {
+  const testUserEmail = 'test@test.com'
+
+  const token = await generateTokenPromise(testUserEmail)
+
+  expect(token).toBeDefined()
+})
