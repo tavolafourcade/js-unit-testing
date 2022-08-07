27c2f1ecbfedd55cf0a460e6b5c65e5d91f881c8
tavolafourcade <tavolafourcade@gmail.com>
2022-08-07 12:04:07 -0500

    [ ADD ] Writing an Integration Test & Reasoning

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,26 +1,39 @@
-import { it, expect } from 'vitest'
-import { transformToNumber } from './numbers'
+import { describe, it, expect } from 'vitest'
+import { transformToNumber, cleanNumbers } from './numbers'
 
-it('should transform a string number to a number of type number', () => {
-  const value = '1'
-  const result = transformToNumber(value)
-  expect(result).toBe(+value)
+describe('transformToNumber()', ()=> {
+  it('should transform a string number to a number of type number', () => {
+    const value = '1'
+    const result = transformToNumber(value)
+    expect(result).toBe(+value)
+  })
+  
+  it('should transform a string number to a number of type number', () => {
+    const value = '1'
+    const result = transformToNumber(value)
+    expect(result).toBeTypeOf('number')
+  })
+  
+  it('should yield NaN for non-transformable values', () => {
+    const value = 'hola'
+    const value2 = {}
+  
+    const result = transformToNumber(value)
+    const result2 = transformToNumber(value2)
+  
+    // Multiple expectations does make sense here
+    expect(result).toBeNaN()
+    expect(result2).toBeNaN()
+  })
 })
 
-it('should transform a string number to a number of type number', () => {
-  const value = '1'
-  const result = transformToNumber(value)
-  expect(result).toBeTypeOf('number')
-})
 
-it('should yield NaN for non-transformable values', () => {
-  const value = 'hola'
-  const value2 = {}
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string number values is provided', () => {
+    const numbersValues = ['1', '2', '3']
+    
+    const cleanedNumbers = cleanNumbers(numbersValues)
 
-  const result = transformToNumber(value)
-  const result2 = transformToNumber(value2)
-
-  // Multiple expectations does make sense here
-  expect(result).toBeNaN()
-  expect(result2).toBeNaN()
+    expect(cleanedNumbers[0]).toBeTypeOf('number')
+  })
 })
