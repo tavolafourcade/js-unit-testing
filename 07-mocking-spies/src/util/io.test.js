62638386c35f8399faa0ed254d2ed2a66bb9b421
tavolafourcade <tavolafourcade@gmail.com>
2022-08-08 22:16:43 -0500

    [ ADD ] Testing the writeFile method with side effect (affecting test.txt)

---
+++ 07-mocking-spies/src/util/io.test.js
@@ -0,0 +1,8 @@
+import { expect, it } from 'vitest'
+import writeData from './io'
+
+it('should execute the writeFile method', () => {
+  const testData = 'Test'
+  const testFileName = 'test.txt'
+  return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+})
