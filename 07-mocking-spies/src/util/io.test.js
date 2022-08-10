1596353ad30f29fe227ee37ee3253741ebbb700f
tavolafourcade <tavolafourcade@gmail.com>
2022-08-09 21:52:33 -0500

    [ ADD ] Getting Started with Mocks & Automocking for functionality that we dont own

--- 07-mocking-spies/src/util/io.test.js
+++ 07-mocking-spies/src/util/io.test.js
@@ -1,8 +1,14 @@
-import { expect, it } from 'vitest'
+import { expect, it, vi } from 'vitest'
+import { promises as fs } from 'fs'
 import writeData from './io'
+
+vi.mock('fs')
 
 it('should execute the writeFile method', () => {
   const testData = 'Test'
   const testFileName = 'test.txt'
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+
+  writeData(testData, testFileName)
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+  expect(fs.writeFile).toBeCalled()
 })
