5d77faeb91afac6cbfa92ff5585a88cc4b376fb0
tavolafourcade <tavolafourcade@gmail.com>
2022-08-13 21:16:35 -0500

    [ ADD should throw an error if a non empty string is provided with only blank as a value

--- 01-starting-project/util/validation.test.js
+++ 01-starting-project/util/validation.test.js
@@ -8,3 +8,11 @@ it('should throw an error if an empty string is provided as a value', () => {
 
   expect(validationFn).toThrow()
 })
+
+it('should throw an error if a non empty string is provided with only blank as a value', () => {
+  const testInput = '   '
+
+  const validationFn = () => validateNotEmpty(testInput) //pattern for testing for errors
+
+  expect(validationFn).toThrow()
+})
