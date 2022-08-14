85976f5f3244bf739af0d3eb82d458790f7b36d5
tavolafourcade <tavolafourcade@gmail.com>
2022-08-13 22:10:46 -0500

    [ ADD ] should return any available response data

---
+++ 01-starting-project/util/http.test.js
@@ -0,0 +1,26 @@
+import { it, vi, expect } from 'vitest'
+import { sendDataRequest } from './http'
+
+const testResponseData = {
+  testKey: 'testData'
+}
+
+const testFetch = vi.fn((url, options)=>{
+  return new Promise((resolve, reject)=>{
+    const testResponse = {
+      ok: true,
+      json(){
+        return new Promise((resolve, reject)=>{
+          resolve(testResponseData)
+        })
+      }
+    }
+    resolve(testResponse)
+  })
+}) // spy functionality
+vi.stubGlobal('fetch', testFetch)
+
+it('should return any available response data', ()=> {
+  const testData = {key: 'test'}
+  return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData)
+})
