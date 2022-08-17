e38d1981c2385f1285e59f198ad4c44a4e34b285
tavolafourcade <tavolafourcade@gmail.com>
2022-08-16 22:13:52 -0500

    [ ADD ] Validating if the fetch correctly convert the values with JSON.stringify

--- 01-starting-project/util/http.test.js
+++ 01-starting-project/util/http.test.js
@@ -7,6 +7,9 @@ const testResponseData = {
 
 const testFetch = vi.fn((url, options)=>{
   return new Promise((resolve, reject)=>{
+    if (typeof options.body !== 'string') {
+      return reject('Not a string')
+    }
     const testResponse = {
       ok: true,
       json(){
@@ -23,4 +26,17 @@ vi.stubGlobal('fetch', testFetch)
 it('should return any available response data', ()=> {
   const testData = {key: 'test'}
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData)
+})
+
+it('should convert the provided data to JSON before sending the request', async ()=> {
+  const testData = {key: 'test'}
+
+  let errorMessage
+  try {
+    await sendDataRequest(testData)
+  } catch (error) {
+    errorMessage = error
+  }
+
+  expect(errorMessage).not.toBe('Not a string')
 })
