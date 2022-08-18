943b754671433612283e3e55170a3504bd2a94cd
tavolafourcade <tavolafourcade@gmail.com>
2022-08-17 21:47:53 -0500

    [ ADD ] Mockcing function in selected tests

--- 01-starting-project/util/http.test.js
+++ 01-starting-project/util/http.test.js
@@ -1,5 +1,6 @@
 import { it, vi, expect } from 'vitest'
 import { sendDataRequest } from './http'
+import { HttpError } from './errors'
 
 const testResponseData = {
   testKey: 'testData'
@@ -39,4 +40,25 @@ it('should convert the provided data to JSON before sending the request', async 
   }
 
   expect(errorMessage).not.toBe('Not a string')
+})
+
+it('should throw an HttpError in case of non-ok responses', async ()=> {
+  testFetch.mockImplementationOnce((url, options)=>{
+    return new Promise((resolve, reject)=>{
+      const testResponse = {
+        ok: false,
+        json(){
+          return new Promise((resolve, reject)=>{
+            resolve(testResponseData)
+          })
+        }
+      }
+      resolve(testResponse)
+    })
+  })
+  const testData = {key: 'test'}
+  
+
+  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError)
+
 })
