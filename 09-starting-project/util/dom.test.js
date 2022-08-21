8f049651ac978754f7cb0f1480ad67c504264ebc
tavolafourcade <tavolafourcade@gmail.com>
2022-08-20 23:03:39 -0500

    [ ADD ] Setting up a Virtual HTML Page

--- 09-starting-project/util/dom.test.js
+++ 09-starting-project/util/dom.test.js
@@ -1,6 +1,18 @@
-import { it } from 'vitest'
+import fs from 'fs'
+import path from 'path' //Open a file and download its content (open and load index HTML file)
+
+import { it, vi } from 'vitest'
+import { Window } from 'happy-dom'
 import { showError } from './dom'
 
+const htmlDocPath = path.join(process.cwd(), 'index.html') // path.join(__dirname, 'index.html')
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString() // The content of the index.html file
+
+const window = new Window() // Create an emulated browser
+const document = window.document // Get the document object
+document.write(htmlDocumentContent) // Write the content of the index.html file to the document (renders the page in the browser)
+
+vi.stubGlobal('document', document) // Replace the global document object with the one we created
 
 it('first test', ()=> {
   showError('test')
