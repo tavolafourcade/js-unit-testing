84cffeb22ee1a574f18b22ee24e3583e13dd7bbe
tavolafourcade <tavolafourcade@gmail.com>
2022-08-22 09:05:35 -0500

    [ ADD ] Implementation of beforeEach to reset the innerHTML body

--- 09-starting-project/util/dom.test.js
+++ 09-starting-project/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path' //Open a file and download its content (open and load index HTML file)
 
-import { it, vi, expect } from 'vitest'
+import { it, vi, expect, beforeEach } from 'vitest'
 import { Window } from 'happy-dom'
 import { showError } from './dom'
 
@@ -10,10 +10,14 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString() // The conte
 
 const window = new Window() // Create an emulated browser
 const document = window.document // Get the document object
-document.write(htmlDocumentContent) // Write the content of the index.html file to the document (renders the page in the browser)
 
 vi.stubGlobal('document', document) // Replace the global document object with the one we created
 
+beforeEach(()=>{
+  document.body.innerHTML = ''
+  document.write(htmlDocumentContent) // Write the content of the index.html file to the document (renders the page in the browser)
+
+})
 it('should add an error paragraph to the id="errors" element', ()=> {
   showError('Test')
 
@@ -21,4 +25,11 @@ it('should add an error paragraph to the id="errors" element', ()=> {
   const errorParagraph = errorsEl.firstElementChild
 
   expect(errorParagraph).not.toBeNull() // Check if the element exists
+})
+
+it('should not contain an error paragraph initially', () => {
+  const errorsEl = document.getElementById('errors')
+  const errorParagraph = errorsEl.firstElementChild
+
+  expect(errorParagraph).toBeNull() // Check if the element exists
 })
